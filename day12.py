802607cee7598e477d15bef139acadaccb055a9a
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-10-31 15:01:25 +0300

    game control and left healt control funcs added

--- day12.py
+++ day12.py
@@ -7,32 +7,34 @@ print("1 ile 100 arasında bir sayı tahmin edeceğiz. 1 ve 100 dahil")
 gameContinue = True
 gameDifficult = ""
 gameNumber = 0
-
 playerNumber = 0
-while gameContinue:
-    gameNumber = random.randint(1, 100)
-    def controlYourNumber(number):
+leftHealt = 10
+def controlEasytoHard(choseeGame):
+    if choseeGame == "zor":
+        return 5
+    elif choseeGame == "kolay":
+        return 10
+    else:
+        return 0
+def controlYourNumber(number,kalanHak):
         if number > gameNumber:
             print("cevabınız çok yüksek")
             print("tekrar deneyiniz")
+            return kalanHak - 1
         elif number < gameNumber:
             print("cevabınız çok düşük")
             print("tekrar deneyiniz")
+            return kalanHak - 1
         else:
             print("Doğru bildiniz. Tebrikler")
             gameContinue = False
-    gameDifficult = input("oyun zor mu kolay mı olsun : ").lower()
-    leftHealt = 10
-    while leftHealt > 0 :
-        if gameDifficult == "zor":
-            leftHealt = 5
-        elif gameDifficult == "kolay":
-            leftHealt = 10
-        else:
-            print("yanlış bir ifade girdiniz")
-            break
-        print(f"oyunu bitirmek için {leftHealt} hakkınız var. o zaman başlayalım")
-        playerNumber = int(input("Tahmin giriniz : "))
-        controlYourNumber(playerNumber)
-        leftHealt -= 1
-        print(leftHealt)
+            return kalanHak
+while gameContinue:
+    gameNumber = random.randint(1, 100)
+    gameDifficult = input("Lütfen zorluk seviyesini seçiniz. kolay yada zor ? : ").lower()
+    leftHealt = controlEasytoHard(gameDifficult)
+    if leftHealt == 0:
+        print("yanlış seviye seçtiğiniz için oyun sona erdi")
+        break
+    
+    
