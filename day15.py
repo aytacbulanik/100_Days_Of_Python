34682c753f07acd78cf52730ed2e30e4df460a67
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-11 16:44:12 +0300

    coffee calculate but not stop

--- day15.py
+++ day15.py
@@ -1,22 +1,31 @@
 from day15Main import resources
 from day15Main import MENU
+
 moneyArray = ["quarter","dime","nickel","penny"]
 coin = 0.0
-def getResources(resource):
-    for resor in resource:
-        print(f"{resor} : {resource[resor]} ml")
+
 def calculateMoney(penny,dime,nickle,quarter):
     coin = (penny * 0.01) + (dime * 0.1) + (nickle * 0.05) + (quarter * 0.25)
     return coin 
+
+def calculateResources(name):
+    if name == "report":
+        for resor in resources:
+            print(f"{resor} : {resources[resor]} ml")
+    elif name == "espresso" or "latte" or "cappucino":
+        for resource in ["water","milk","coffee"]:
+            if resources[resource] < MENU[name]["ingredients"][resource]:
+                print(f"Yeterli {resource} yok")
+            else:
+                quartersMoney = float(input("How many quarters ? :"))
+                dimesMoney = float(input("How many dimes ? :"))
+                nicklesMoney = float(input("How many nickles ? :"))
+                penniesMoney = float(input("How many pennies ? :"))
+                coin = calculateMoney(penniesMoney,dimesMoney,nicklesMoney,quartersMoney)
+                coffeeCost = MENU[whatDoYouWant]["cost"]
+                print(coin - coffeeCost)
+                for resource in ["water","milk","coffee"]:
+                    resources[resource] -= MENU[name]["ingredients"][resource]
 whatDoYouWant = ""
-whatDoYouWant = input("Ne içmek istersiniz ? (espresso , latte , cappucino)")
-if whatDoYouWant == "report":
-    getResources(resources)
-elif whatDoYouWant == "espresso" or "latte" or "cappucino":
-    quartersMoney = float(input("How many quarters ? :"))
-    dimesMoney = float(input("How many dimes ? :"))
-    nicklesMoney = float(input("How many nickles ? :"))
-    penniesMoney = float(input("How many pennies ? :"))
-    coin = calculateMoney(penniesMoney,dimesMoney,nicklesMoney,quartersMoney)
-    coffeeCost = MENU[whatDoYouWant]["cost"]
-    print(coin - coffeeCost)
+whatDoYouWant = input("Ne içmek istersiniz ? (espresso , latte , cappucino) : ")
+calculateResources(whatDoYouWant)
