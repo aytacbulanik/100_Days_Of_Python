47a26a3ab1a654c0270e6a4446244c13641c7372
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-11 09:32:03 +0300

    money cost and return

--- day15.py
+++ day15.py
@@ -1,7 +1,22 @@
 from day15Main import resources
-
+from day15Main import MENU
+moneyArray = ["quarter","dime","nickel","penny"]
+coin = 0.0
+def getResources(resource):
+    for resor in resource:
+        print(f"{resor} : {resource[resor]} ml")
+def calculateMoney(penny,dime,nickle,quarter):
+    coin = (penny * 0.01) + (dime * 0.1) + (nickle * 0.05) + (quarter * 0.25)
+    return coin 
 whatDoYouWant = ""
 whatDoYouWant = input("Ne içmek istersiniz ? (espresso , latte , cappucino)")
-if whatDoYouWant == "resource":
-    for resor in resources:
-        print(f"{resor} : {resources[resor]}")
+if whatDoYouWant == "report":
+    getResources(resources)
+elif whatDoYouWant == "espresso" or "latte" or "cappucino":
+    quartersMoney = float(input("How many quarters ? :"))
+    dimesMoney = float(input("How many dimes ? :"))
+    nicklesMoney = float(input("How many nickles ? :"))
+    penniesMoney = float(input("How many pennies ? :"))
+    coin = calculateMoney(penniesMoney,dimesMoney,nicklesMoney,quartersMoney)
+    coffeeCost = MENU[whatDoYouWant]["cost"]
+    print(coin - coffeeCost)
