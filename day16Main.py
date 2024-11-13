f7301290fd17b2374e12769c8c45136bb98f71d1
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-13 14:25:19 +0300

    day 16 started

---
+++ day16Main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle ,Screen 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_row
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("blue1")
+timmy.forward(100)
+myScreen = Screen()
+myScreen.exitonclick()
+print(myScreen.canvheight)
