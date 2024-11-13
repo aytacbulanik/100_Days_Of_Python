9ce4da9d6a2d3fa622a28d0ee550048de925f6c0
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-13 15:51:23 +0300

    coffee machine start with classes

--- day16Main.py
+++ day16Main.py
@@ -1,12 +1,7 @@
-from turtle import Turtle ,Screen 
-from prettytable import PrettyTable
+from prettytable import PrettyTable 
 
 table = PrettyTable()
-table.add_row
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("blue1")
-timmy.forward(100)
-myScreen = Screen()
-myScreen.exitonclick()
-print(myScreen.canvheight)
+
+table.add_column("Ad", ["ali","aytaç","mehmet","yeşim"])
+table.add_column("Soyad", ["mehemt","kimya","öztürk","sönmez"], align="l")
+print(table)
