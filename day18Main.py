715996398072b14d6fbeec84d08d01c4bd39c1ba
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-21 19:42:09 +0300

    kenar sayısı doğru ama çalışmıyor

--- day18Main.py
+++ day18Main.py
@@ -2,8 +2,13 @@ from turtle import Turtle , Screen
 
 myTurtle = Turtle()
 myScreen = Screen()
-myTurtle.shape("square")
-myTurtle.color("blue")
+myTurtle.shape("arrow")
+myTurtle.color(0.4,0.8,0.9)
+kenarSayisi = 3
+donusAcisi = 360 / kenarSayisi
+for _ in range(5):
+    for _ in range(kenarSayisi):
+        myTurtle.forward(50)
+        myTurtle.right(donusAcisi)
+    kenarSayisi += 1
 myScreen.exitonclick()
-
-
