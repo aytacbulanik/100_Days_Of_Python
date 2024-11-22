2feaa94e598a5020059d21b5802e0fbcd24353f7
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-11-22 12:49:57 +0300

    kenar sayısı doğru çalışıyor ve rengi değişiyor

--- day18Main.py
+++ day18Main.py
@@ -1,14 +1,17 @@
 from turtle import Turtle , Screen
+import random
 
 myTurtle = Turtle()
 myScreen = Screen()
 myTurtle.shape("arrow")
 myTurtle.color(0.4,0.8,0.9)
 kenarSayisi = 3
-donusAcisi = 360 / kenarSayisi
-for _ in range(5):
+
+for _ in range(6):
     for _ in range(kenarSayisi):
+        donusAcisi = 360 / kenarSayisi
         myTurtle.forward(50)
         myTurtle.right(donusAcisi)
     kenarSayisi += 1
+    myTurtle.color(random.random(),random.random(),random.random())
 myScreen.exitonclick()
