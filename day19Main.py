d963bffa764be1fe24c51053c37ee365e6a9976c
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-12-13 09:48:14 +0300

    myTurtle move four way

--- day19Main.py
+++ day19Main.py
@@ -2,3 +2,18 @@ from turtle import Turtle , Screen
 
 myTurtle = Turtle()
 myScreen = Screen()
+
+def moveForward():
+    myTurtle.forward(30)
+def moveBackward():
+    myTurtle.backward(30)
+def turnClock():
+    myTurtle.left(90)
+def turnRightClockwise():
+    myTurtle.right(70)
+myScreen.listen()
+myScreen.onkey(moveForward, "w")
+myScreen.onkey(moveBackward, "s")
+myScreen.onkey(turnClock, "a")
+myScreen.onkey(turnRightClockwise, "d")
+myScreen.exitonclick()
