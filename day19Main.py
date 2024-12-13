071ca27bc044e246068d76a61d123375f4783a2c
AYTAÇ BULANIK <aytacbulanik@hotmail.com>
2024-12-13 10:15:25 +0300

    myturtle move when i want anywhere

--- day19Main.py
+++ day19Main.py
@@ -4,16 +4,23 @@ myTurtle = Turtle()
 myScreen = Screen()
 
 def moveForward():
-    myTurtle.forward(30)
+    myTurtle.forward(20)
 def moveBackward():
-    myTurtle.backward(30)
+    myTurtle.backward(20)
 def turnClock():
-    myTurtle.left(90)
+    myTurtle.left(10)
 def turnRightClockwise():
-    myTurtle.right(70)
+    myTurtle.right(10)
+def clearScreen():
+    myTurtle.clear()
+    myTurtle.penup()
+    myTurtle.setheading(0)
+    myTurtle.setpos(0,0)
+    myTurtle.pendown()
 myScreen.listen()
 myScreen.onkey(moveForward, "w")
 myScreen.onkey(moveBackward, "s")
 myScreen.onkey(turnClock, "a")
 myScreen.onkey(turnRightClockwise, "d")
+myScreen.onkey(clearScreen,"c")
 myScreen.exitonclick()
