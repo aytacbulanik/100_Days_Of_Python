20bfe8083112a941471eb31b8fb43f149f9b6aea
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-10-13 15:44:51 +0300

    recursive func added in day26

--- day26Main.py
+++ day26Main.py
@@ -1,8 +1,16 @@
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-name = "aytac".upper()
-spellName = []
+data = pandas.read_csv("./files/nato_phonetic_alphabet.csv")
+
 newDict = { row.letter : row.code for (index , row) in data.iterrows()}
-spellName = [ newDict[letter] for letter in name]
-print(spellName)
+def writeAlphabetic():
+    name = input("Enter a word : ").upper()
+    try:
+        spellName = [ newDict[letter] for letter in name]
+    except:
+        print("Sorry, only letters in the alphabet")
+        writeAlphabetic() #fonksiyon içerisine aynı fonksiyonu koyarsak recursive fonk kullanmış oluruz. otomatik döngü oluşur.
+    else:
+        print(spellName)
+
+writeAlphabetic()
