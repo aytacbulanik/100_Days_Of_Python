5acfa526ca1d92c868eddf11aee7f096e6cb5438
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-10-28 00:04:36 +0300

    new question is showing

--- day31Main.py
+++ day31Main.py
@@ -3,25 +3,39 @@ from random import choice
 from tkinter import *
 
 BGCOLOR = "#B1DDC6"
-data = pandas.read_csv("./files/test.csv")
+data = pandas.read_csv("./files/day31/test.csv")
 #burası çok önemli data frame de olan her bir satırı dict nesnesine çeviriyor
 dataDict = data.to_dict(orient="records")
+currentWord = {}
 def newQuestion():
+    global currentWord , flipTimer
+    window.after_cancel(flipTimer)
     currentWord = choice(dataDict)
-    canvas.itemconfig(titleText , text = "English")
-    canvas.itemconfig(questionText, text = currentWord["english"])
+    canvas.itemconfig(titleText , text = "English", fill="white")
+    canvas.itemconfig(questionText, text = currentWord["english"], fill= "white")
+    canvas.itemconfig(cardBG, image=bgImage)
+    flipTimer = window.after(3000,func=changeCard)
+
+def changeCard():
+    canvas.itemconfig(titleText , text= "Turkish", fill="black")
+    canvas.itemconfig(questionText , text = currentWord["turkish"],fill="black")
+    canvas.itemconfig(cardBG, image=bgFrontImage)
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50,bg=BGCOLOR)
-bgImagePath = "./files/day31/card_front.png"
+
+flipTimer = window.after(3000,func=changeCard)
+bgImagePath = "./files/day31/card_back.png"
+bgFrontImagePath = "./files/day31/card_front.png"
 trueButtonPath = "./files/day31/right.png"
 falseButtonPath = "./files/day31/wrong.png"
 bgImage = PhotoImage(file=bgImagePath)
+bgFrontImage = PhotoImage(file=bgFrontImagePath)
 trueImage = PhotoImage(file=trueButtonPath)
 falseImage = PhotoImage(file=falseButtonPath)
 canvas = Canvas(width=820,height=560,highlightthickness=0,bg=BGCOLOR)
-canvas.create_image(410,280,image=bgImage)
+cardBG = canvas.create_image(410,280,image=bgImage)
 canvas.grid(row=0,column=0,columnspan=2)
 
 titleText = canvas.create_text(400,200,text="English",font=("Arial",30,"italic"),fill="black")
