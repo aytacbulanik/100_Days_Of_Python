3ae9835637d1d1b077c683800422784718e5cacd
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-10-29 00:08:13 +0300

    save new data csv when the answer is true

--- day31Main.py
+++ day31Main.py
@@ -3,23 +3,38 @@ from random import choice
 from tkinter import *
 
 BGCOLOR = "#B1DDC6"
-data = pandas.read_csv("./files/day31/test.csv")
-#burası çok önemli data frame de olan her bir satırı dict nesnesine çeviriyor
-dataDict = data.to_dict(orient="records")
-currentWord = {}
-def newQuestion():
-    global currentWord , flipTimer
+currentCard = {} #uygulamada o an gösterilen nesneyi başka boş şekilde global
+#olarka tanımlıyoruz. sonra sürekli erişmek için
+dataDict = {} #bunu global olarak tanımlıyoruzki başka değerde atıyalım
+try:
+    data = pandas.read_csv("./files/day31/newWord.csv")
+except FileNotFoundError:
+    originalData = pandas.read_csv("./files/day31/englishToTurkish1000.csv")
+    dataDict = originalData.to_dict(orient="records")
+else:
+    #burası çok önemli data frame de olan her bir satırı dict nesnesine çeviriyor
+    dataDict = data.to_dict(orient="records") 
+
+def nextCard():
+    global currentCard , flipTimer
     window.after_cancel(flipTimer)
-    currentWord = choice(dataDict)
+    currentCard = choice(dataDict)
     canvas.itemconfig(titleText , text = "English", fill="white")
-    canvas.itemconfig(questionText, text = currentWord["english"], fill= "white")
+    canvas.itemconfig(questionText, text = currentCard["english"], fill= "white")
     canvas.itemconfig(cardBG, image=bgImage)
     flipTimer = window.after(3000,func=changeCard)
 
 def changeCard():
     canvas.itemconfig(titleText , text= "Turkish", fill="black")
-    canvas.itemconfig(questionText , text = currentWord["turkish"],fill="black")
+    canvas.itemconfig(questionText , text = currentCard["turkish"],fill="black")
     canvas.itemconfig(cardBG, image=bgFrontImage)
+
+def isTrue():
+    dataDict.remove(currentCard)
+    data = pandas.DataFrame(dataDict)
+    data.to_csv("files/day31/newWord.csv",index=False) #bu satır çok öenmli 
+    #yeni bir csv oluştururken başına sıra numarası eklemiyor.
+    nextCard()
 
 window = Tk()
 window.title("Flashy")
@@ -34,20 +49,18 @@ bgImage = PhotoImage(file=bgImagePath)
 bgFrontImage = PhotoImage(file=bgFrontImagePath)
 trueImage = PhotoImage(file=trueButtonPath)
 falseImage = PhotoImage(file=falseButtonPath)
-canvas = Canvas(width=820,height=560,highlightthickness=0,bg=BGCOLOR)
-cardBG = canvas.create_image(410,280,image=bgImage)
+canvas = Canvas(width=800,height=560,highlightthickness=0,bg=BGCOLOR)
+cardBG = canvas.create_image(400,280,image=bgImage)
 canvas.grid(row=0,column=0,columnspan=2)
 
 titleText = canvas.create_text(400,200,text="English",font=("Arial",30,"italic"),fill="black")
 questionText = canvas.create_text(400,350,text="Word",font=("Arial",40,"bold"),fill="black")
-trueButton = Button(image=trueImage,highlightthickness=0,command=newQuestion)
+trueButton = Button(image=trueImage,highlightthickness=0,command=isTrue)
 trueButton.grid(row=1,column=0)
 
-falseButton = Button(image=falseImage,highlightthickness=0,command=newQuestion)
+falseButton = Button(image=falseImage,highlightthickness=0,command=nextCard)
 falseButton.grid(row=1,column=1)
 
-newQuestion()
-
-
+nextCard()
 
 window.mainloop()
