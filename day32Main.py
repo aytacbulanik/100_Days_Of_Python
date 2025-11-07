f593d89767999c3afaa17553124ca2ca7a2d477e
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-07 15:36:09 +0300

    send new motivation mail in mondays

--- day32Main.py
+++ day32Main.py
@@ -1,16 +1,3 @@
-# import smtplib
-
-# myEmail = "itouchbulanik@gmail.com" #maili atacak adresimiz
-# password = "zravhkinmoijelhi" #uygulama için mail e tanımladığım şifrem
-# with smtplib.SMTP("smtp.gmail.com") as connection:#google stmp server e erişiyoruz.
-#     connection.starttls() #bu kodla bir güvenlik katmanı oluşturuyoruz.
-#     connection.login(user=myEmail, password=password)
-#     connection.sendmail(from_addr=myEmail, 
-#                         to_addrs="aytacbulanik@hotmail.com" , 
-#                         msg="Subject:Hello\n\n My first email")# Subject parametresi maile konu ekliyor
-#                         #Böylece spama düşmüyor.
-
-
 # import datetime as dt
 
 # now = dt.datetime.now() #şu anki saati almak için bunu giriyoruz. now değişkenini
@@ -23,4 +10,31 @@
 
 # print(myBirthDay)
 
+#hantanın gününü kontrol ederek pazartesi ise motivasyon maili atan bir uygulama yazdık
+import datetime as dt
+import pandas
+import smtplib
+from random import choice
+data = pandas.read_csv("./files/day32/quotes.txt")
+dataList = data.to_dict(orient="records")
 
+def getNewWords():
+    return choice(dataList)
+isDayMonday = dt.datetime.now().weekday()
+
+def sendEmail(message):
+    myEmail = "itouchbulanik@gmail.com" #maili atacak adresimiz
+    password = "zravhkinmoijelhi" #uygulama için mail e tanımladığım şifrem
+    with smtplib.SMTP("smtp.gmail.com") as connection:#google stmp server e erişiyoruz.
+        connection.starttls() #bu kodla bir güvenlik katmanı oluşturuyoruz.
+        connection.login(user=myEmail, password=password)
+        connection.sendmail(from_addr=myEmail, 
+                            to_addrs="aytacbulanik@hotmail.com" , 
+                            msg=f"Subject:Hello\n\n {message}")# Subject parametresi maile konu ekliyor
+                            #Böylece spama düşmüyor.
+
+if isDayMonday == 1:
+    newWord = getNewWords()["words"]
+    sendEmail(newWord)
+else:
+    print("Bugün değil")
