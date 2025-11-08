cab6a649439877b2c84b84fdf86e99d90173f7d2
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-09 01:22:41 +0300

    kayne west app completed

--- day33Main.py
+++ day33Main.py
@@ -1,1 +1,14 @@
 import requests
+
+#aşağıdaki satırla bir url e istek atıyoruz. bunu response değişkenine atıyoruz.
+response = requests.get(url="http://api.open-notify.org/iss-now.json") 
+response.raise_for_status() #hata kodunu yakalamak için bunu yazıyoruz.
+data = response.json() #json nesnesini değişkene atıyoruz.
+
+#json nesnesinin alt değerlerine ulaşıyoruz.
+latitude = data["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
+
+issPosition = (longitude,latitude)
+
+print(issPosition)
