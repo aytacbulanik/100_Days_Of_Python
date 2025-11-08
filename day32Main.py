3fac0007e314280f29af991071abdf449b25c6fe
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-08 20:43:08 +0300

    day 32 completed

--- day32Main.py
+++ day32Main.py
@@ -1,27 +1,8 @@
-# import datetime as dt
-
-# now = dt.datetime.now() #şu anki saati almak için bunu giriyoruz. now değişkenini
-# #kullanarak istediğimiz tarih kısmına ulaşabiliriz.
-# print(now.year) #geçerli tarihten yılı alıyoruz.
-# print(now.weekday()) #geçerli haftanın günün alıyoruz.
-
-# #geçerli bir tarih ve zaman değerini nesne olarak oluşturup saklayabiliriz.
-# myBirthDay = dt.datetime(year=1981,month=12,day=4,hour=16,minute=30)
-
-# print(myBirthDay)
-
 #hantanın gününü kontrol ederek pazartesi ise motivasyon maili atan bir uygulama yazdık
 import datetime as dt
 import pandas
 import smtplib
 from random import choice
-
-# data = pandas.read_csv("./files/day32/quotes.txt")
-# dataList = data.to_dict(orient="records")
-
-# def getNewWords():
-#     return choice(dataList)
-# isDayMonday = dt.datetime.now().weekday()
 
 def sendEmail(message):
     myEmail = "itouchbulanik@gmail.com" #maili atacak adresimiz
@@ -35,11 +16,6 @@ def sendEmail(message):
                             # Subject parametresi maile konu ekliyor
                             #Böylece spama düşmüyor.
 
-# if isDayMonday == 1:
-#     newWord = getNewWords()["words"]
-#     sendEmail(newWord)
-# else:
-#     print("Bugün değil")
 
 with open("./files/day32/birthdays.txt") as data:
     newData = pandas.read_csv(data)
