adf770d3ca5dda8f02ec59f212a736805d76d037
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-16 16:22:25 +0300

    i can take condition ID

--- day35Main.py
+++ day35Main.py
@@ -2,12 +2,19 @@ import requests
 apiKey = "3ab9c00817b1ff26d099dc919895bdf8"
 
 parameters = {
-    "lat" : 36.71,
-    "lon" : 37.10,
-    "appid" : apiKey
+    "lat" : 45.93,
+    "lon" : 5.8,
+    "appid" : apiKey,
+    "cnt" : 4, #sadece 4 veriyi eçkiyor sınır koymuş oluyoruz
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast",
                         params=parameters)
+response.raise_for_status()
+listData = response.json()["list"]
 
-print(response.json()["list"][0]["weather"][0]["description"])
+for dayly in listData:
+    newId = dayly["weather"][0]["id"]
+    if newId > 500:
+        print("You must take a shamp")
+        break
