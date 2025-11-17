36ac1feba89ae7ead0c90cc67d53225cf040240f
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-17 16:37:33 +0300

    send a message with twilio

--- day35Main.py
+++ day35Main.py
@@ -1,5 +1,9 @@
 import requests
+from twilio.rest import Client
+
 apiKey = "3ab9c00817b1ff26d099dc919895bdf8"
+account_sid = "AC936ac74c343ddc9f0b3eb7d9dcb25b9d"
+auth_token = "2081cf3226af4f0ab0e17312de310e4c"
 
 parameters = {
     "lat" : 45.93,
@@ -13,8 +17,20 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast",
 response.raise_for_status()
 listData = response.json()["list"]
 
+willRain = False
 for dayly in listData:
     newId = dayly["weather"][0]["id"]
     if newId > 500:
-        print("You must take a shamp")
-        break
+        willRain = True    
+if willRain:
+    
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+    from_='whatsapp:+14155238886',
+    content_sid='HX229f5a04fd0510ce1b071852155d3e75',
+    content_variables='{"1":"409333"}',
+    to='whatsapp:+905342447474'
+    )
+
+    print(message.sid)
