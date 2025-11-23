bd735b8b8fe6eba7629a3e8f282b9718e6500dd2
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-11-23 21:14:04 +0300

    day 38 finished

--- day38Main.py
+++ day38Main.py
@@ -1,18 +1,23 @@
 import requests
 from datetime import datetime as dt
 
+#antreman verisini girecek kişinin genele bilgileri değişken olarak alınıyor.
 GENDER = "male"
 WEIGHT_KG = 80
 HEIGHT_CM = 174
 AGE = 44
+#api keyler
 xappid = "app_500abcfe0b8d45fa84852786"
 xappkey = "nix_live_1oW8hv05jKPP9EeESMZaY2XX8yUhzRHG" #"x-app-key"
+#zamanı tarih ve saat olarak ayırıyoruz.
 now = dt.now()
 nowDate = now.strftime("%d/%m/%Y")
 nowTime = now.strftime("%H:%M:%S")
+#end pointler
 baseEndpoint = "https://app.100daysofpython.dev"
 excersizeEndpoint = f"{baseEndpoint}/v1/nutrition/natural/exercise"
 sheetEndpoint = "https://api.sheety.co/426a997d2a4c936fab96ea547b7071dd/myProject/works"
+#endpointlere istek atarken kullanılan headerslar
 headers = {
     "x-app-id" : xappid,
     "x-app-key" : xappkey
@@ -21,7 +26,7 @@ headers = {
 bearer_headers = {
     "Authorization": "Basic YXl0YWNidWxhbmlrOkE5ODQ2OTg0NmE/"
 }
-
+#antreman api için json ile post isteği atıyoruz.
 dataJson = {
     "query" : "swam for 1 hour",
     "weight_kg" : WEIGHT_KG,
@@ -32,6 +37,7 @@ dataJson = {
 
 response = requests.post(url=excersizeEndpoint,json=dataJson,headers=headers)
 result = response.json()
+#cevap olarak dönen veri de işimize yarayan verileri alıp google sheet e yazacağız.
 for exercise in result["exercises"]:
     sheet_inputs = {
         "work": {
