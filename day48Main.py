3191686560b460f46a3c997978d2a471eda993f3
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-12-14 17:25:33 +0300

    form submit with selenium

--- day48Main.py
+++ day48Main.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By #By modülünü import etme
-
+from selenium.webdriver.common.keys import Keys #Keys modülünü import etme
+import time
 #otomatik kapanmayı engelleme
 chromeOptions = webdriver.ChromeOptions()
 chromeOptions.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chromeOptions) #option parametresini gönderiyoruz
-driver.get("https://www.python.org/")
+# driver.get("https://www.python.org/")
 # # priceTL = driver.find_element(By.CLASS_NAME, value="a-price-whole") #sınıf adına göre elementi bulma
 # # priceKurus = driver.find_element(By.CLASS_NAME, value="a-price-fraction") #sınıf adına göre elementi bulma
 # # print(f"Fiyat: {priceTL.text},{priceKurus.text} TL")
@@ -22,13 +23,28 @@ driver.get("https://www.python.org/")
 # bugLink = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[2]/a')
 # print(bugLink.text) #elementin textini yazdırma
 
-eventLink = driver.find_elements(By.CSS_SELECTOR , value=".event-widget li a") #event-widget class'ı altındaki tüm li etiketlerindeki a etiketlerini bulma
-enentTime = driver.find_elements(By.CSS_SELECTOR , value=".event-widget time") #event-widget class'ı altındaki tüm li etiketlerindeki time etiketlerini bulma
-events = {}
-for n in range(len(eventLink)):
-    events[n] = {
-        "time": enentTime[n].text,
-        "name": eventLink[n].text
-    }
-print(events)
-driver.quit()
+# eventLink = driver.find_element(By.CSS_SELECTOR , value=".event-widget li a") #event-widget class'ı altındaki tüm li etiketlerindeki a etiketlerini bulma
+# enentTime = driver.find_elements(By.CSS_SELECTOR , value=".event-widget time") #event-widget class'ı altındaki tüm li etiketlerindeki time etiketlerini bulma
+# events = {}
+# for n in range(len(eventLink)):
+#     events[n] = {
+#         "time": enentTime[n].text,
+#         "name": eventLink[n].text
+#     }
+# print(events)
+# driver.quit()
+# linkGo = driver.find_element(By.LINK_TEXT , value="Python Meeting Düsseldorf") #link text kullanarak elementi bulma
+# linkGo.click() #elemente tıklama işlemi
+
+# driver.get("https://tr.wikipedia.org/wiki/Anasayfa")
+# search = driver.find_element(By.NAME , value="search")
+# search.send_keys("Python",Keys.ENTER) #elemente metin gönderme işlemi
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+fName = driver.find_element(By.NAME , value="fName")
+fName.send_keys("John")
+lName = driver.find_element(By.NAME , value="lName")
+lName.send_keys("Waiting")
+email = driver.find_element(By.NAME , value="email")
+email.send_keys("a@domain.com")
+button = driver.find_element(By.XPATH, value='/html/body/form/button')
+button.click()
--- practice.py
+++ practice.py
@@ -1,13 +1,61 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By #By modülünü import etme
+import random
+# name = "aytaç bulanık"
+# if "a" not in name:
+#     print("The letter 'a' is present in the name.")
 
-#otomatik kapanmayı engelleme
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_experimental_option("detach", True)
+# number = 12.753
+# rounded_number = round(number,2)
+# print("Rounded number:", rounded_number)
 
-driver = webdriver.Chrome(options=chromeOptions) #option parametresini gönderiyoruz
-driver.get("https://tr.wikipedia.org/wiki/Anasayfa")
+# newList = [1, 2, 3, 4, 5]
+# newList.append(6)
+# print("Updated list:", newList)
 
-articelsCount = driver.find_element(By.CSS_SELECTOR, value=".main-plainlist b a")
-print(articelsCount.text)
-driver.quit()
+# newDict = {
+#     "name": "aytaç",
+#     "age": 25,
+#     "city": "istanbul"
+# }
+# print(type(newDict["age"]))
+# newDict["gender"] = "Fmale"
+# print("Updated dictionary:", newDict)
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#         self.salary = 100000.0
+    
+#     def greet(self):
+#         print(f"Merhaba {self.name}")
+
+#     def updateSalary(self,point):
+#         self.salary *= point
+
+# class Employee(Person):
+#     def __init__(self,name,age,position):
+#         super().__init__(name,age)
+#         self.position = position
+# person1 = Person("aytaç",44)
+# emp1 = Employee("kamil",44,"developer")
+# emp1.updateSalary(0.6)
+# person1.updateSalary(1.2)
+
+# print(person1.salary)
+# print(emp1.salary)
+
+# numbers = [0,1,2,3,4,5,6,7,8,9,10]
+# newNumbers = [ n for n in numbers if n%2 == 0]
+# print(newNumbers)
+# list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+# intList = [ int(n) for n in list_of_strings ]
+# oddList = [n for n in intList if n%2 == 0]
+# print(oddList.)
+
+# names = ["ali","hülya","ahmet","mehmet","nimet"]
+# newDict = { name : random.randint(1,100) for name in names}
+# print(newDict)
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weatherF = { day : (degrees*0.2)+273 for (day,degrees) in weather_c.items()}
+print(weatherF)
