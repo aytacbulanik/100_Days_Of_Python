e4be21ba35c4bcac39eadcaaf45a546fffdcbf76
Bulanık Aytaç <itouchbulanik@gmail.com>
2025-12-21 18:22:18 +0300

    path use with flask

--- day54Main.py
+++ day54Main.py
@@ -6,4 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-hello_world()
+@app.route("/bye")
+def bye_world():
+    return "<p>Goodbye, World!</p>"
+
+#username değişkeni tanımlayarak kullanıcı adını dinamik olarak alır
+@app.route('/user/<username>/<int:age>')
+def show_user_profile(username, age):
+    return f'Hello : {username} how are you? You are {age} years old.'
+
+if __name__ == "__main__":
+    app.run(debug=True) #debug=True ile uygulama hata ayıklama modunda çalışır.
+    # Bu, kodda yapılan değişikliklerin otomatik olarak algılanmasını sağlar.
